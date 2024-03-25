10c8be7418a5094bdabea7927893784ca7ae06e2
Sagar~ <sagargope2001@gmail.com>
2024-03-25 12:23:18 +0530

    (python):updated notes

---
+++ 01_basics/chai.py
@@ -0,0 +1,3 @@
+from hello_chai import kuchbhi;
+
+kuchbhi("chai")
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+def kuchbhi(n):
+    print(n)
+
+kuchbhi("sagar")
+
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala chai"
